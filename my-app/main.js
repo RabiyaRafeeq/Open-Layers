5dac4b87d539b04974f09eb21fb8793f28eecd57
RabiyaRafeeq <50367671+RabiyaRafeeq@users.noreply.github.com>
2022-02-28 18:39:19 +0530

    Add files via upload

--- my-app/main.js
+++ my-app/main.js
@@ -1,4 +1,4 @@
-import 'ol/ol.css';
+import './style.css';
 import {Map, View} from 'ol';
 import TileLayer from 'ol/layer/Tile';
 import OSM from 'ol/source/OSM';
@@ -9,28 +9,25 @@ import Modify from 'ol/interaction/Modify';
 import GeoJSON from 'ol/format/GeoJSON';
 import Feature from 'ol/Feature';
 import Polygon from 'ol/geom/Polygon';
+import FeatureController from './services/FeatureService';
+import FeatureService from './services/FeatureService';
 
+const featureService = new FeatureService();
 
+const map = new Map({
+  target: 'map',
+  layers: [
+    new TileLayer({
+      source: new OSM()
+    })
+  ],
+  view: new View({
+    center: [0, 0],
+    zoom: 2
+  })
+}); 
 
-    const raster = new TileLayer({
-      source: new OSM(),
-    });
-    
-    const source = new VectorSource();
-    
-    const vector = new VectorLayer({
-      source: source,
-    });
-    
-    const map = new Map({
-      layers: [raster, vector],
-      target: 'map',
-      view: new View({
-        center: [-11000000, 4600000],
-        zoom: 4,
-      })
-    });
-    var homeButton=document.createElement("button")
+var homeButton=document.createElement("button")
 homeButton.innerHTML='<img src="Graphicloads-100-Flat-Home.ico",alt="image" width:"100" height="50" >'
     homeButton.className="myButton"
     var homeElement=document.createElement("div")
@@ -43,113 +40,126 @@ homeButton.innerHTML='<img src="Graphicloads-100-Flat-Home.ico",alt="image" widt
         location.href="index.html"
     })
     map.addControl(homeControl)
-    
+    const vectorSource = new VectorSource();
+    const vector=new VectorLayer({
+      source:vectorSource
+    })
+    var note = new ol.control.Notification();
+    map.addControl(note)
 
+    // Add the editbar
+    var select = new ol.interaction.Select({ title: 'Sélection'});
+    select.set('title', 'Sélection');
+    var edit = new ol.control.EditBar({
+      // Translate interaction title / label 
+      interactions: { 
+        // Use our own interaction > set the title inside
+        Select: select,
+        // Define button title
+        DrawLine: 'Ligne',
+        // Drawregular with label
+        DrawRegular: { title: 'Forme régullière', ptsLabel: 'pts', circleLabel: 'cercle' }
+      },
+      source: vectorSource
+    });
+    map.addControl(edit);
 
-            var LineButton=document.createElement("button")
-            LineButton.innerHTML='<img src="draw-line-with-anchors-line-Icon.png",alt="image" width:"128" height="52" >'
-                LineButton.className="myButton"
-                var LineElement=document.createElement("div")
-                LineElement.className="LineButtonDiv"
-                LineElement.appendChild(LineButton)
-                var LineControl=new ol.control.Control({
-                    element:LineElement
-                })
-                LineButton.addEventListener("click",()=>{
-                  addInteraction();
-                  function addInteraction() {
-                      var draw = new Draw({
-                        source: source,
-                        type: 'LineString',
-                      });
-                      const modify = new Modify({
-                        source:source
-                      });
-                      
-                      map.addInteraction(draw);
-                      map.addInteraction(modify);
-                    }
-                  })
-                map.addControl(LineControl)
+    // Add a tooltip
+    var tooltip = new ol.Overlay.Tooltip();
+    map.addOverlay(tooltip);
 
-
-
-                var PolygonButton=document.createElement("button")
-                PolygonButton.innerHTML='<img src="719-7197135_primary-kig-polygon-clip-arts-draw-polygon-icon.png",alt="image" width:"120" height="50" >'
-                    PolygonButton.className="myButton"
-                    var PolygonElement=document.createElement("div")
-                    PolygonElement.className="PolygonButtonDiv"
-                    PolygonElement.appendChild(PolygonButton)
-                    var PolygonControl=new ol.control.Control({
-                        element:PolygonElement
-                    })
-                    PolygonButton.addEventListener("click",()=>{
-                      addInteraction();
-                      function addInteraction() {
-                          var draw = new Draw({
-                            source: source,
-                            type: 'Polygon',
-                          });
-                          const modify = new Modify({
-                            source:source
-                          });
-                          
-                          map.addInteraction(draw);
-                          map.addInteraction(modify);
-                        }
-                      })
-                    map.addControl(PolygonControl)
-
-            var Button=document.createElement("button")
-            Button.innerHTML='<img src="png-clipart-google-map-maker-google-maps-drawing-pin-google-search-you-are-here-angle-pin.png",alt="image" width:"45" height="29" >'
-                Button.className="myButton"
-                var Element=document.createElement("div")
-                Element.className="ButtonDiv"
-                Element.appendChild(Button)
-                var Control=new ol.control.Control({
-                    element:Element
-                })
-                Button.addEventListener("click",()=>{
-                  addInteraction();
-                  function addInteraction() {
-                      var draw = new Draw({
-                        source: source,
-                        type: 'Point',
-                      });
-                      const modify = new Modify({
-                        source:source
-                      });
-                      
-                      map.addInteraction(draw);
-                      map.addInteraction(modify);
-                    }
-                  })
-                map.addControl(Control)
-              
-                
-            var Button=document.createElement("button")
-            Button.innerHTML='<img src="2479618.png",alt="image" width:"100" height="50" >'
-                Button.className="myButton"
-                var Element=document.createElement("div")
-                Element.className="ButtonDiv"
-                Element.appendChild(Button)
-                var Control=new ol.control.Control({
-                    element:Element
-                })
-                Button.addEventListener("click",()=>{
-                  addInteraction();
-                  function addInteraction() {
-                      var draw = new Draw({
-                        source: source,
-                        type: 'Circle',
-                      });
-                      const modify = new Modify({
-                        source:source
-                      });
-                      
-                      map.addInteraction(draw);
-                      map.addInteraction(modify);
-                    }
-                  })
-                map.addControl(Control)
+    edit.getInteraction('Select').on('select', function(e){
+      if (this.getFeatures().getLength()) {
+        tooltip.setInfo('Drag points on features to edit...');
+      }
+      else tooltip.setInfo();
+    });
+    edit.getInteraction('Select').on('change:active', function(e){
+      tooltip.setInfo('');
+    });
+    edit.getInteraction('ModifySelect').on('modifystart', function(e){
+      if (e.features.length===1) tooltip.setFeature(e.features[0]);
+    });
+    edit.getInteraction('ModifySelect').on('modifyend', function(e){
+      tooltip.setFeature();
+    });
+    edit.getInteraction('DrawPoint').on('change:active', function(e){
+      tooltip.setInfo(e.oldValue ? '' : 'Click map to place a point...');
+    });
+    edit.getInteraction('DrawLine').on(['change:active','drawend'], function(e){
+      tooltip.setFeature();
+      tooltip.setInfo(e.oldValue ? '' : 'Click map to start drawing line...');
+    });
+    edit.getInteraction('DrawLine').on('drawstart', function(e){
+      tooltip.setFeature(e.feature);
+      tooltip.setInfo('Click to continue drawing line...');
+    });
+    edit.getInteraction('DrawPolygon').on('drawstart', function(e){
+      tooltip.setFeature(e.feature);
+      tooltip.setInfo('Click to continue drawing shape...');
+    });
+    edit.getInteraction('DrawPolygon').on(['change:active','drawend'], function(e){
+      tooltip.setFeature();
+      tooltip.setInfo(e.oldValue ? '' : 'Click map to start drawing shape...');
+   
+      var draw = new Draw({
+        source: vectorSource,
+        type: 'Polygon'
+      });
+      const modify = new Modify({
+        source:vectorSource
+      });
       
+      map.addInteraction(draw);
+      map.addInteraction(modify);
+      modify.on("modifyend",e=>{
+        const modifiedfeature=e.features.getArray()[0];
+        featureService.updateFeature(modifiedfeature);
+      })
+      
+      vectorSource.on("addfeature",e=>{
+        featureService.saveFeature(e.feature);
+      })
+    });
+    edit.getInteraction('DrawHole').on('drawstart', function(e){
+      tooltip.setFeature(e.feature);
+      tooltip.setInfo('Click to continue drawing hole...');
+    });
+    edit.getInteraction('DrawHole').on(['change:active','drawend'], function(e){
+      tooltip.setFeature();
+      tooltip.setInfo(e.oldValue ? '' : 'Click polygon to start drawing hole...');
+    });
+    edit.getInteraction('DrawRegular').on('drawstart', function(e){
+      tooltip.setFeature(e.feature);
+      tooltip.setInfo('Move and click map to finish drawing...');
+    });
+    edit.getInteraction('DrawRegular').on(['change:active','drawend'], function(e){
+      tooltip.setFeature();
+      tooltip.setInfo(e.oldValue ? '' : 'Click map to start drawing shape...');
+    });
+
+
+    edit.on('info', function(e){
+      console.log(e)
+      note.show('<i class="fa fa-info-circle"></i> '+e.features.getLength()+' feature(s) selected');
+    });
+   
+window.addEventListener("load",async (e)=>{
+ const savedFeatures = await featureService.getFeatures();
+ console.log(savedFeatures)
+ savedFeatures.forEach(f=>{
+   const feature = new Feature({
+     geometry:new Polygon(f.geometry.coordinates),
+     type:f.type,
+     _id:f._id
+   })
+   vectorSource.addFeature(feature);
+   
+ })
+ 
+})
+map.addLayer(vector);
+
+
+
+
