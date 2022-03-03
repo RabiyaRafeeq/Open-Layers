83b4c6af77c2bc32643c8018786c70f3eaf3a615
RabiyaRafeeq <50367671+RabiyaRafeeq@users.noreply.github.com>
2022-03-03 19:10:18 +0530

    Create app.js

---
+++ my-app server/app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const cors = require('cors');
+const DataLoadRouter = require('./routes/DataLoadRouter');
+const AmplifierRouter = require('./routes/AmplifierRouter');
+
+const app = new express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/api/uploadShape",DataLoadRouter);
+app.use("/api/uploadAmplifer",AmplifierRouter);
+app.get("/api/ping",(req,res)=>{
+    res.status(200).json({"state":"live"});
+})
+module.exports = app;
