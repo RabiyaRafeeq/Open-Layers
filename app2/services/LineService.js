3574099534fa6cbd68beea07d770972996d5660b
RabiyaRafeeq <50367671+RabiyaRafeeq@users.noreply.github.com>
2022-03-13 13:48:18 +0530

    Add files via upload

---
+++ app2/services/LineService.js
@@ -0,0 +1,31 @@
+import GeoJSON from 'ol/format/GeoJSON';
+import LineModel from "../model/Line";
+
+class LineService{
+    lineModel = new LineModel();
+    async saveLine(feature){
+        if(!feature.values_._id){
+            let writer = new GeoJSON();
+            let data = writer.writeFeatureObject(feature)
+            data.type = 'LineString';
+            const res=await this.lineModel.saveLine(data);
+            feature.values_._id=res._id
+          }
+    }
+
+    getLines(){
+        const features = this.lineModel.getLines();
+        return features;
+    }
+
+    updateLine(feature){
+        const id=feature.values_._id
+        const geom={geometry:{
+            type:"LineString",
+            coordinates:feature.getGeometry().getCoordinates()
+        }}
+        this.lineModel.putLine(id,geom);
+    }
+}
+
+export default LineService;
